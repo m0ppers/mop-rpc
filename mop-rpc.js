5c782e32c97b3442524b785edb4579e3e1b021ba
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-01-23 15:12:43 +0100

    Fix strict errors

--- mop-rpc.js
+++ mop-rpc.js
@@ -1,11 +1,12 @@
+"use strict";
 var MopRpcBrowserConnectionAdapter = require("./MopRpcBrowserConnectionAdapter");
 var MopRpcWebsocketConnectionAdapter = require("./MopRpcWebsocketConnectionAdapter");
 
 module.exports = {
     "createAdapter": function(connection) {
-        if (window && window.WebSocket && connection && connection instanceof window.WebSocket) {
+        if (typeof window != "undefined" && window.WebSocket && connection && connection instanceof window.WebSocket) {
             return new MopRpcBrowserConnectionAdapter(connection);
-        } else if (window === undefined && connection && typeof connection.socket == "object") {
+        } else if (typeof window == "undefined" && typeof connection && typeof connection.socket == "object") {
             return new MopRpcWebsocketConnectionAdapter(connection);
         } else {
             throw new Error("Unknown connection. Must create adapter manually!");
